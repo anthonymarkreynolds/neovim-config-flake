a303f5d9d0ddf8200f5841167c62c14517103b98
anthonymarkreynolds <anthonymarkreynolds@outlook.com>
2023-05-18 08:12:33 +1000

    update plugin generation method

--- flake.nix
+++ flake.nix
@@ -2,59 +2,66 @@
   description = "Neovim Config Flake";
   inputs = {
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-22.11";
+    nixpkgs-unstable.url = "github:NixOS/nixpkgs/nixos-unstable";
   };
 
-  outputs = { self, nixpkgs }:
-  let
-    system = "x86_64-linux";
-    pkgs = nixpkgs.legacyPackages.${system};
-    pluginList = import ./plugins-list.nix;
-    buildVimPlugin = { name, url, sha256 }: pkgs.vimUtils.buildVimPluginFrom2Nix {
-      pname = name;
-      version = "latest";
-      src = builtins.fetchTarball {
-        inherit url sha256;
+  outputs = { self, nixpkgs, nixpkgs-unstable }:
+    let
+      system = "x86_64-linux";
+      pkgs = import nixpkgs {
+        inherit system;
       };
-    };
-    myVimPlugins = (map buildVimPlugin pluginList);
-
-  in {
-    packages.${system} = {
-      neovim-custom-config = pkgs.stdenv.mkDerivation {
-        name = "neovim-custom-config";
-        src = self;
-          installPhase = ''
-          mkdir -p $out/
-          cp -r ./nvim $out/
-          '';
+      unstablePkgs = import nixpkgs-unstable {
+        inherit system;
+      };
+      nvimPath = ./nvim;
+      pluginList = import ./plugins-list.nix;
+      buildVimPlugin = src@{ repo, ... }: pkgs.vimUtils.buildVimPluginFrom2Nix {
+        pname = repo;
+        version = "latest";
+        src = pkgs.fetchFromGitHub src;
       };
 
-      neovim-custom = pkgs.neovim.override {
-        configure = {
-          customRC = ''
-            let g:custom_config_path = '${self.packages.${system}.neovim-custom-config}/nvim'
-            exec 'luafile ' . g:custom_config_path . '/init.lua'
-          '';
-          packages.myVimPackage = with pkgs.vimPlugins; {
-            # add plugins from nixpkgs here
-            start = [
-              # nvim-treesitter.withAllGrammars
-            ] ++ myVimPlugins;
+    in {
+      packages.${system} = {
+        neovim-custom-config = pkgs.stdenv.mkDerivation {
+          name = "neovim-custom-config";
+          src = self;
+            installPhase = ''
+            mkdir -p $out/
+            cp -r ./nvim $out/
+            '';
+        };
+
+        neovim-custom = pkgs.neovim.override {
+          configure = {
+            customRC = ''
+              let g:custom_config_path = '${self.packages.${system}.neovim-custom-config}/nvim'
+              exec 'luafile ' . g:custom_config_path . '/init.lua'
+            '';
+            packages.myVimPackage = with pkgs.vimPlugins; {
+              # add plugins from nixpkgs here
+              start = [
+                # nvim-treesitter.withAllGrammars
+              ] ++ (map buildVimPlugin pluginList);
+            };
           };
         };
+
+        update-plugins = let
+          pythonEnv = pkgs.python3.withPackages (ps: [
+              ps.requests
+          ]);
+          prefetchGithub = pkgs.nix-prefetch-github;
+        in pkgs.writeScriptBin "update-plugins" ''
+          #!${pkgs.runtimeShell}
+          export PATH=${prefetchGithub}/bin:$PATH
+          ${pythonEnv}/bin/python ./generate-plugins-list.py
+        '';
       };
 
-      update-plugins = let
-        pythonEnv = pkgs.python3.withPackages (ps: [
-            ps.requests
-        ]);
-      in pkgs.writeScriptBin "update-plugins" ''
-        #!${pkgs.runtimeShell}
-        ${pythonEnv}/bin/python ${./generate-plugins-list.py}
-      '';
+    defaultPackage.${system} = self.packages.${system}.neovim-custom;
 
     };
 
-    defaultPackage.${system} = self.packages.${system}.neovim-custom;
-  };
 }
